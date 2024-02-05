eedf66f22f7805ff3e545ccc1576996b02351f83
panagoa <panagoa@ya.ru>
2024-02-05 04:31:58 +0300

    build presentation

---
+++ presentations/01_intro_for_jarez/assets/channel-649fb47b-kUIfxL63.js
@@ -0,0 +1,1 @@
+import{bU as r,bV as a}from"./index-Vz1PnNvQ.js";const s=(n,o)=>r.lang.round(a.parse(n)[o]),e=s;export{e as c};
---
+++ presentations/01_intro_for_jarez/assets/clone-0d7cbc00-HHkd7D06.js
@@ -0,0 +1,1 @@
+import{j as r}from"./graph-03626afa-QvChyZiU.js";var e=4;function a(o){return r(o,e)}export{a as c};
